b20cd3f75a96ce014cfa3aa1ced9f70fe241ad0b
Jannes Hühnerbein <j.huehnerbein@icloud.com>
2022-11-10 18:24:39 +0100

    fixed a bug that occured when using

--- src/IndexedStructs.jl
+++ src/IndexedStructs.jl
@@ -11,9 +11,13 @@ function _find_struct_name(expr)
     if expr.head == :struct
         if expr.args[2] isa Symbol
             return expr.args[2]
-        elseif expr.args[2] isa Expr
-            return expr.args[2].args[1]
         end
+
+        s_name = expr.args[2]
+        while s_name isa Expr
+            s_name = s_name.args[1]
+        end
+        return s_name
     elseif expr.head == :macrocall
         name = _find_struct_name(expr.args[3])
         if name !== nothing
