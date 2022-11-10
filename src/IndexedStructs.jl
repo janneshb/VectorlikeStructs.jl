5f3d361feaef426e780b1d459a1c3bd57136f7e1
Jannes Hühnerbein <j.huehnerbein@icloud.com>
2022-11-10 15:38:20 +0100

    @indexed can now handle other macros being appended to a struct definition

--- src/IndexedStructs.jl
+++ src/IndexedStructs.jl
@@ -3,11 +3,43 @@ module IndexedStructs
 export @indexed
 
 macro indexed(T)
-esc(:(
+    expr = _indexify(T)
+    return esc(expr)
+end
+
+function _find_struct_name(expr)
+    if expr.head == :struct
+        return expr.args[2]
+    elseif expr.head == :macrocall
+        name = _find_struct_name(expr.args[3])
+        if name !== nothing
+            return name
+        else
+            error("Could not find a struct definition in what was passed to @indexed.")
+        end
+    end
+    return nothing
+end
+
+function _indexify(expr)
+    T = _find_struct_name(expr)
+
+    return quote
+        eval($expr)
+        function Base.getindex(t::S, i::I) where {S <: $T, I}
+            getfield(t, fieldnames($T)[i])
+        end
+    end
+end
+
+#=
+push!(qs, quote
     function Base.getindex(t::S, i::I) where {S <: $T, I}
         getfield(t, fieldnames($T)[i])
     end
-))
-end
+end)
+
+return qs
+=#
 
 end # module IndexedStructs
